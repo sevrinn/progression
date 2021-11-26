1ad197173e2bd927f72c65778471124febd8a101
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-26 11:11:57 -0800

    config server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// import express and other libraries
+const express = require('express');
+
+// configure express server
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// configure mongoose to connect
+require ('./config/mongoose.config');
+
+// add routes to listen for
+const userRoutes = require('./routes/user.routes');
+userRoutes(app);
+
+// star the app server listening
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
