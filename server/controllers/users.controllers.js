260d7e184047b7c23d8fb96602b00ff5d0db3879
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-30 16:35:09 -0800

    added comments to controller

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,15 +1,13 @@
 
 const User = require('../models/user.model')
 
-const getAllUsers = (req, res) => {
-  User.find()
-  .then((allUsers) => res.json({ users: allUsers }))
-  .catch((err) => res.json({ errMsg: err }))
-};
-
+// controller function to add new user to user database
 const addNewUser = (req, res) => {
   console.log("in new user")
+  // destructrures body from req
   const { body } = req;
+  // create() must be fed an object whose keys MUST match 
+  // with the field names in the model
   User.create({
     firstName: body.firstName,
     lastName: body.lastName,
@@ -17,26 +15,61 @@ const addNewUser = (req, res) => {
     email: body.email,
     password: body.password
   })
+  // send json response to client with new user that was just added
   .then((newUser) => res.json({newUser: newUser}))
-  .catch((err) => res.json({errorMessage: err}));
-}
+  .catch((err) => {
+    // if there is an error in db query, send status of 400 and json response w/ error msg
+    res.status(400).json({errorMessage: err})
+  });
+};
 
+// controller function that queries all users
+const getAllUsers = (req, res) => {
+  // find() gets all docs from db
+  User.find()
+  // send json response with all users to client
+  .then((allUsers) => res.json({ users: allUsers }))
+  .catch((err) => {
+    res.status(400).json({ errMsg: err })
+  });
+};
+
+// controller function that queries a single restaurant by its id
 const getUserById = (req, res) => {
   console.log(req.params.userId)
+  // findOne() takes obj as arg {queryField: queryVal}
   User.findOne({ _id: req.params.userId })
+  // if successfull return json response to client w/ user info
   .then((user) => res.json({user: user}))
-  .catch((err) => res.send(err))
-}
+  // if there's an error in db query, send status 400 and json response w/ error msg
+  .catch((err) => {
+    res.status(400).send({errMsg: err})
+  });
+};
 
+
+// controller to update user
 const updateUser = (req, res) => {
+  // findeOneAndUpdate takes 1st arg: {queryField: queryVal}, 2nd arg: the fields to update 
+  // (req.body contains form data as an object)
+  // 3rd arg: settings. new: true means we want updated doc supplied in arg in .then promise block
   User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true, runValidators: true})
+  // if successful send json response w/ updated user
   .then((updatedUser) => res.json(updatedUser))
-  .catch((err) => res.send(err))
-}
+  // if there's an error in query, send status 400 and json response w/ error msg
+  .catch((err) => {
+    res.status(400).send(err)
+  });
+};
 
+// controller to delete user
 const deleteUser = (req, res) => {
+  // findOneAndDelete takes object as arg {queryField: queryVal}
   User.findOneAndDelete({_id: req.params.userId})
-  .then((deleteUser) => res.json(deleteUser))
+  // if successful send 200 status code and message user deleted
+  .then((deleteUser) => {
+    res.status(200).json(deleteUser).send("User Deleted")
+  })
   .catch((err) => res.json(err))
 }
 
